f8cb6d20b644f77a2724357da39d1b27824a2efb
DataBadSmells <TheBigOneCalledBitey@protonmail.com>
2024-01-24 14:37:44 +0000

    remove file

--- utils/split_csv.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-
-in_csv = 'data/CIC/Tuesday-WorkingHours.csv'
-size = 46000
-
-with pd.read_csv(in_csv, chunksize=size) as reader:
-    for i,chunk in enumerate(reader):
-        chunk.to_csv(f'data/example/Tuesday_CIC_{i}.csv', index=False, header=True)
