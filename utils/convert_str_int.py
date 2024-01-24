13b8fc16c6c03ea038f70577bb84316b39113304
DataBadSmells <TheBigOneCalledBitey@protonmail.com>
2024-01-24 14:37:13 +0000

    remove file

--- utils/convert_str_int.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-df = pd.read_csv('data/example/MNIST/mnist_test.csv')
-df['label'] = df['label'].apply(str)
-df.to_csv("data/example/MNIST/mnist_str_test.csv", header=True, index=False)
